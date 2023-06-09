951ad4037f0754e76bb6764d2ca906e671f3f205
Harshal Khairnar <khairnar2960@gmail.com>
2023-06-09 13:38:50 +0530

    support added for custom padding character

--- js-printf.js
+++ js-printf.js
@@ -2,7 +2,7 @@ function sprintf(format) {
   var args = Array.prototype.slice.call(arguments, 1);
   var index = 0;
 
-  return format.replace(/%(\d+\$)?(0?)(\d+)?(\.\d+)?([a-zA-Z%])/g, function (match, argIndex, zeroPad, width, precision, specifier) {
+  return format.replace(/%(\d+\$)?([0-9]*)?(\.?[0-9]+)?([a-zA-Z%])/g, function (match, argIndex, width, precision, specifier) {
     if (match === '%%') return '%';
 
     // If the argument index is provided in the format string
@@ -10,7 +10,7 @@ function sprintf(format) {
       var idx = parseInt(argIndex, 10);
       if (idx <= args.length) {
         var arg = args[idx - 1];
-        return processSpecifier(specifier, arg, zeroPad, width, precision);
+        return processSpecifier(specifier, arg, width, precision);
       }
     }
 
@@ -18,13 +18,13 @@ function sprintf(format) {
     // fallback to sequential argument retrieval
     if (index < args.length) {
       var arg = args[index++];
-      return processSpecifier(specifier, arg, zeroPad, width, precision);
+      return processSpecifier(specifier, arg, width, precision);
     }
 
     return match;
   });
 
-  function processSpecifier(specifier, arg, zeroPad, width, precision) {
+  function processSpecifier(specifier, arg, width, precision) {
     var stringValue;
 
     switch (specifier) {
@@ -47,10 +47,16 @@ function sprintf(format) {
     // Apply padding if width is specified
     if (width) {
       var padLength = parseInt(width, 10) - stringValue.length;
-      var paddingChar = zeroPad ? '0' : ' ';
+      var paddingChar = ' ';
+      
+      // Check if a custom padding character is provided
+      if (width.length > 1 && width[0] === '0' && width[1] !== '.') {
+        paddingChar = width[1];
+      }
+
       if (padLength > 0) {
         var padding = paddingChar.repeat(padLength);
-        if (zeroPad && (stringValue[0] === '-' || stringValue[0] === '+')) {
+        if ((paddingChar === '0') && (stringValue[0] === '-' || stringValue[0] === '+')) {
           // Preserve sign when zero-padding
           stringValue = stringValue[0] + padding + stringValue.slice(1);
         } else {
@@ -66,42 +72,3 @@ function sprintf(format) {
 function vsprintf(format, args) {
   return sprintf.apply(null, [format].concat(args));
 }
-
-/*
-
-Uses:
-
-var name = 'John';
-var age = 25;
-var height = 1.852;
-var message = sprintf('My name is %s, I am %d years old, and my height is %f meters.', name, age, height);
-console.log(message);
-
-var message2 = sprintf('My name is %2$s, I am %1$d years old, and my height is %f meters.', age, name, height);
-console.log(message2);
-
-var pi = 3.14159;
-console.log(sprintf('The value of pi is approximately %.2f.', pi));
-
-console.log(sprintf('The value of pi is approximately %0.2f.', pi));
-
-
-var message3 = sprintf('Hello, %10s!', name);
-console.log(message3);  // Output: Hello,       John!
-
-var message4 = sprintf('Age: %02d', age);
-console.log(message4);  // Output: Age: 25
-
-var message5 = sprintf('Value of pi: %010.2f', pi);
-console.log(message5);  // Output: Value of pi: 000003.14
-
-Output:
-
-My name is John, I am 25 years old, and my height is 1.85 meters.
-
-My name is John, I am 25 years old, and my height is 1.85 meters.
-
-The value of pi is approximately 3.14.
-
-The value of pi is approximately 3.14.
-*/
